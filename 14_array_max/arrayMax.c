2b61b66fcac78c2bfcab7aca5be4865431a8016b
Coursera Learner <nobody@nowhere.nul>
2020-10-17 17:39:56 +0000

    Did assignment 14

--- 14_array_max/arrayMax.c
+++ 14_array_max/arrayMax.c
@@ -2,7 +2,17 @@
 #include <stdlib.h>
 
 int * arrayMax(int * array, int n) {
-  return NULL;
+  if (n <= 0) {
+    return NULL;
+  }
+  int largest = array[0];
+  for (int i = 1; i < n; i++) {
+    if (array[i] > largest) {
+      largest = array[i];
+    }
+  }
+  int * ptr = &largest;
+  return ptr;
 }
 
 void doTest(int * array, int n) {
