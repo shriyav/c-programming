827d0e64743bd6dab085ff2c3b8c73a77aba2faa
Coursera Learner <nobody@nowhere.nul>
2020-10-19 11:10:59 +0000

    Did assignment 16

--- 16_subseq/maxSeq.c
+++ 16_subseq/maxSeq.c
@@ -2,21 +2,41 @@
 #include <stdio.h>
 
 size_t maxSeq(int * array, size_t n) {
-  int highest = 0;
-  int counter = 1;
-  for (int i = 1; i <= n; i++) {
+  size_t highest=0;
+  int counter=1;
+
+  if (n<0) {
+    return -1;
+  }
+
+  if (array == NULL) {
+    return 0;
+  }
+  
+  if (n==1) {
+    return 1;
+  }
+  
+  for (int i = 1; i < n; i++) {
     if (array[i] > array[i-1]) {
 	counter ++;
-      }
-    else if (counter > highest) {
-      highest = counter;
-      counter = 1;
+	printf("counter value is: %d\n", counter);
+	if (counter>highest) {
+	  highest=counter;
+	}
     }
     else {
-      counter = 1;
+	  if (counter > highest) {
+	    highest = counter;
+	    counter = 1;
+	  }
+	  else {
+	    counter = 1;
+	  }
+	  printf("counter value is: %d\n", counter);
     }
   }
-  printf("This is the highest: %d\n", highest);
+  printf("Got this: %lu.\n", highest);
   return highest;
 }
 
